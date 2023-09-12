a1b63e9d1340929c5793773712661522f15be034
Romysaa-ali-hashim <roma.ali.8225@gmail.com>
2023-09-12 16:58:23 -0700

    dtu

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - int _abs(int)
+ * @i: value use to find the absolute value.
+ *
+ *
+ * Return: the absolute value.
+ */
+int _abs(int i)
+{
+if (i < 0)
+return (-i);
+return (i);
+}
