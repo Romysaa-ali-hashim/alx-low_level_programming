ba868af9fbf06c23404d458c763a23af2dee7b6e
Romysaa-ali-hashim <roma.ali.8225@gmail.com>
2023-09-12 12:38:59 -0700

    w

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point 
+ *prints the alphabet, in lowercase 
  *
  * Return: Always 0.
  */
 void print_alphabet(void)
+{
 	int i;
 	for (i=97;i <= 122;i++)
 {
 	_putchar(i);
 }
 	_putchar(\n);
-    return (0);
+	}
